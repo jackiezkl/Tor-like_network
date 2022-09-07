18b4bc9e20aa3182aa4d7d72c3b453bf0021e207
jackiezkl <jackiezkl@users.noreply.github.com>
2022-09-07 11:17:40 -0500

    Update Entrypoint.sh

--- docker_home_folder/Entrypoint.sh
+++ docker_home_folder/Entrypoint.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
 Xvfb :99 -ac &
-#top
 ifconfig  eth0 mtu 1500
 ethtool -K eth0 tx off rx off tso off gso off gro off lro off
 source /root/pyvenv/bin/activate
-python3 /home/action.py $1
+if [[ $2 == "regular" ]]
+then
+    python3 /home/action.py $1
+elif [[ $2 == "tor" ]]
+then
+    python3 /home/tor_action.py $1
+elif [[ $2 == "tor-like" ]]
+then
+    python3 /home/tor_like_action.py $1
+fi
